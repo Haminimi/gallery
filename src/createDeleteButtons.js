e7d8b5271afd59e02ae680bb562472306b990475
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-04 18:51:52 +0100

    Add listeners for delete buttons
    Introduce functionality to delete gallery slides when the delete button
    is clicked. Update the gallery display, including navigation circles
    and buttons, to reflect changes dynamically.

--- src/createDeleteButtons.js
+++ src/createDeleteButtons.js
@@ -39,3 +39,42 @@ export function createDeleteButtons() {
 		});
 	}
 }
+
+function getDeleteButtons() {
+	deleteButtons = document.querySelectorAll('.delete-button');
+}
+
+export function addListenersToDeleteButtons() {
+	getDeleteButtons();
+
+	deleteButtons.forEach((button, index) => {
+		button.addEventListener('click', () => {
+			const parentSlide = button.parentNode;
+			const gallery = parentSlide.parentNode;
+			gallery.removeChild(parentSlide);
+			updateImages();
+
+			if (images.length === 0) {
+				const emptyGallery = document.createElement('div');
+				emptyGallery.classList.add('empty-gallery');
+				emptyGallery.textContent = 'Your gallery is empty 😭';
+				gallery.appendChild(emptyGallery);
+				gallery.removeChild(circlesParent);
+				gallery.removeChild(left);
+				gallery.removeChild(right);
+			}
+
+			circlesParent.innerHTML = '';
+			createCircles('delete');
+
+			if (index === 0) {
+				showImage(0);
+			} else {
+				showImage(index - 1, 'remove');
+			}
+
+			createDeleteButtons();
+			addListenersToDeleteButtons();
+		});
+	});
+}
