50796a7afe143044273833bea61f69ed4666d35c
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-04 18:35:42 +0100

    Get DOM elements

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export let images = document.querySelectorAll('.slide');
+export const left = document.getElementById('left');
+export const right = document.getElementById('right');
+const addImage = document.getElementById('image-input');
+const gitHubIcon = document.querySelector('.fa-github');
