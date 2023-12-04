f6dd2cda3c614a37a7b54004e5ba1a868f6dd0bc
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-04 18:36:18 +0100

    Add event listeners for GitHub icon

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ export const left = document.getElementById('left');
 export const right = document.getElementById('right');
 const addImage = document.getElementById('image-input');
 const gitHubIcon = document.querySelector('.fa-github');
+
+//Github icon animation
+gitHubIcon.addEventListener('mouseover', () => {
+	gitHubIcon.classList.remove('mouseout');
+	gitHubIcon.classList.add('rotate');
+});
+gitHubIcon.addEventListener('mouseout', () => {
+	gitHubIcon.classList.remove('rotate');
+	gitHubIcon.classList.add('mouseout');
+});
